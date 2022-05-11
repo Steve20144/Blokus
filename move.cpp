d5ee9c3058249d03fc1d9e4ab60935f0f1bd887b
efidrogoudi <105382247+efidrogoudi@users.noreply.github.com>
2022-05-11 20:32:42 +0300

    Add files via upload

--- move.cpp
+++ move.cpp
@@ -1,3 +1,35 @@
 #include "move.h"
 
+Move::Move(Piece movePiece, int moveX, int moveY, char moveOrientation, char moveFlip)
+{
+    piece=movePiece;
+    x=moveX;
+    y=moveY;
+    orientation=moveOrientation;
+    flip=moveFlip;
+}
+
+int Move::getX()
+{
+    return x;
+}
+int Move::getY()
+{
+    return y;
+}
+char Move::getOrientation()
+{
+    return orientation;
+}
+char Move::getFlip()
+{
+    return flip;
+}
+
+
+
+
+
+
+
 // TODO: Implement here the methods of Move
