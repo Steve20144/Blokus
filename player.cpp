934f2f2d88471ec11bff6eebbac2c3417377463e
StergiosNtontos <92440728+StergiosNtontos@users.noreply.github.com>
2022-05-11 20:28:03 +0300

    Add files via upload

--- player.cpp
+++ player.cpp
@@ -2,3 +2,25 @@
 #include "player.h"
 
 // TODO: Implement here the methods of Player
+
+Player(int playerId){
+    id=playerId;
+    cout << "Give your name: " << endl;
+    cin >> name;
+    numPlacedPieces = 0;
+    createPieces();
+}
+
+string Player::getName(){
+    return name;
+}
+
+Piece Player::getPiece(int index){
+        return pieces(index);
+}
+
+int Player::getNumberOfAvailablePieces()
+{
+    return 21-numPlacedPieces;
+}
+
