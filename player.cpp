583cae806a03f458d03bb15c1654685145e90c03
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2022-05-25 19:27:54 +0300

    2o-paradoteo

--- player.cpp
+++ player.cpp
@@ -1,28 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include "player.h"
 
-// TODO: Implement here the methods of Player
-
-Player::Player(int playerId)
-{
-    id = playerId;
-    cout << "Give your name: " << endl;
-    cin >> name;
-    numPlacedPieces = 0;
-    createPieces();
-}
-
-string Player::getName()
-{
-    return name;
-}
-
-Piece Player::getPiece(int index)
-{
-    return pieces[index];
-}
-
-int Player::getNumberOfAvailablePieces()
-{
-    return 21 - numPlacedPieces;
-}
+// TODO: Implement here the methods of Player and all derived classes
