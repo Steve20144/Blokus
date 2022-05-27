b8b1bd4ee89bb1e21134e00b36f0df79b026a3c0
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2022-05-27 18:59:08 +0300

    Update boardrules.cpp

--- boardrules.cpp
+++ boardrules.cpp
@@ -4,10 +4,31 @@
 // TODO: Implement here the method pieceCanBePlaced and computeScore of Board
 // Do not implement any other methods of Board (as they are already implemented in file board.cpp)
 
-bool Board::pieceCanBePlaced(Piece* piece, int x, int y) {
+bool Board::pieceCanBePlaced(Piece *piece, int x, int y)
+{
+    if (playerHasPlacedNoPieces(piece->getPlayer()) &&x = 4 &&y = 4)
+        return true;
+    if (playerHasPlacedNoPieces(piece->getPlayer()) &&x = 9 &&y = 9)
+        return true;
+
     return true;
 }
 
-int Board::computeScore(Player* player){
-    return 0;
+int Board::computeScore(Player *player)
+{
+    // a point is given when :
+    // A.When the player places a piece
+    // B.When the player has placed all his pieces, 15 more points are added to his score
+    //  C.If the player has placed all of his pieces but the last piece he placed was the smallest one // he earns an extra of 5 points
+
+    int pointSum = 0;
+
+    pointSum += player->getNumberOfPlacedPieces();
+    if (player->getNumberOfAvailablePieces() == 0)
+        pointSum += 15;
+
+    if (getLastPiecePlayedByPlayer(player)->getId() == 1)
+        pointSum += 5;
+
+    return pointSum;
 }
