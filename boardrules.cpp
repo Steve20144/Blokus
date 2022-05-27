9901249c2afe6dde1a79febdc4555f25bc97eeef
Stefanos Fragoulis <stefanos.fragoulis@outlook.com.gr>
2022-05-27 21:25:22 +0300

    Update boardrules.cpp

--- boardrules.cpp
+++ boardrules.cpp
@@ -5,41 +5,49 @@
 // Do not implement any other methods of Board (as they are already implemented in file board.cpp)
 
 bool Board::pieceCanBePlaced(Piece *piece, int x, int y)
-
 {
-    bool check;
 
     if (playerHasPlacedNoPieces('#') && x == 4 && y == 4)
-        check = true;
+        return true;
 
     if (playerHasPlacedNoPieces('O') && x == 9 && y == 9)
-        check = true;
+        return true;
 
     for (int i = x; i < x + 5; i++)
     {
         for (int k = y; k < y + 5; k++)
         {
             if (!playerHasPlacedNoPieces('#') && (squareBelongsToPlayer(i + 1, k + 1, '#') || squareBelongsToPlayer(i - 1, k - 1, '#') || squareBelongsToPlayer(i - 1, k + 1, '#') || squareBelongsToPlayer(i + 1, k + 1, '#')))
-            {
-                if (!playerHasPlacedNoPieces('#') && (squareBelongsToPlayer(i, k - 1, '#') || squareBelongsToPlayer(i + 1, k, '#') || squareBelongsToPlayer(i, k + 1, '#') || squareBelongsToPlayer(i - 1, k, '#')))
-                    return true;
-            }
+                return true;
+            return false;
             // else
             //     return false;
 
             if (!playerHasPlacedNoPieces('O') && (squareBelongsToPlayer(i + 1, k + 1, 'O') || squareBelongsToPlayer(i - 1, k - 1, 'O') || squareBelongsToPlayer(i - 1, k + 1, 'O') || squareBelongsToPlayer(i + 1, k + 1, 'O')))
-            {
-                if (!playerHasPlacedNoPieces('O') && (squareBelongsToPlayer(i, k - 1, 'O') || squareBelongsToPlayer(i + 1, k, 'O') || squareBelongsToPlayer(i, k + 1, 'O') || squareBelongsToPlayer(i - 1, k, 'O')))
-                    return true;
-            }
+                return true;
+            return false;
             // else
             //     return false;
         }
     }
 
     if (x < 13 && y < 13)
-        check = true;
+        return true;
 
+    for (int i = x; i < x + 5; i++)
+    {
+        for (int k = y; k < y + 5; k++)
+        {
+            if (!playerHasPlacedNoPieces('#') && (squareBelongsToPlayer(i, k - 1, '#') || squareBelongsToPlayer(i + 1, k, '#') || squareBelongsToPlayer(i, k + 1, '#') || squareBelongsToPlayer(i - 1, k, '#')))
+                return true;
+            // else
+            //     return false;
+            if (!playerHasPlacedNoPieces('O') && (squareBelongsToPlayer(i, k - 1, 'O') || squareBelongsToPlayer(i + 1, k, 'O') || squareBelongsToPlayer(i, k + 1, 'O') || squareBelongsToPlayer(i - 1, k, 'O')))
+                return true;
+            // else
+            //     return false;
+        }
+    }
     return false;
 }
 
